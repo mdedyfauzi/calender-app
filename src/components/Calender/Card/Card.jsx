ac172ebd5fa3198271f0b1a2d8747f76619022f7
mdedyfauzi <mdedyfauzi@gmail.com>
2022-04-20 16:02:30 +0700

    feat: add event card

--- src/components/Calender/Card/Card.jsx
+++ src/components/Calender/Card/Card.jsx
@@ -4,9 +4,9 @@ const Card = ({ day = '' | 0, event1, event2, event3 }) => {
   return (
     <div className={day.getMonth() !== 4 ? styles.disabled : styles.card}>
       <p>{day.getDate()}</p>
-      <p>{event1}</p>
-      <p>{event2}</p>
-      <p>{event3}</p>
+      {event1 && <p>{event1}</p>}
+      {event2 && <p>{event2}</p>}
+      {event3 && <p>{event3}</p>}
     </div>
   );
 };
